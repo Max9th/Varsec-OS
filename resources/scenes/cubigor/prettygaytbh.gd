7837f07a40322d9564d4b13d5058605d784847e5
Max9th <154016869+Max9th@users.noreply.github.com>
2024-09-25 23:43:36 -0300

    ?

---
+++ resources/scenes/cubigor/prettygaytbh.gd
@@ -0,0 +1,20 @@
+extends Node2D
+#these are variables:
+@onready var person_theme: AudioStreamPlayer = $persontheme
+var person_mode: bool = false
+#Here, a signal was defined
+signal Disablebk
+
+func person_spawner():  # tells the computer that if he's in person mode, he should emit the signal disablebk, and check if the music really is playing
+	if person_mode:
+		if not person_theme.playing:  
+			person_theme.playing = true #Here, the property "playing" from the "person_theme" node was modified
+		Disablebk.emit() #Emits a signal. Who would've guessed :D
+	else:
+		if person_theme.playing:
+			person_theme.stop()
+
+
+func _on_authenticated() -> void: #this is system generated function that activates when receiving a signal called "authenticated"
+		person_mode = true
+		person_spawner() # Calls the "Person spawner" function
